40794637b33b006c79c9e7f732f0c97c6f73cef6
zw <18755489161@163.com>
2018-07-10 17:00:04 +0800

    将引入element-tree的组件放在他本身的vue中

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@
 import Vue from "vue";
 import App from "./App";
 import router from "./router";
-import { Tree } from 'element-ui';
-
-Vue.component(Tree.name, Tree);
 import iView from "iview";
 import 'iview/dist/styles/iview.css';
 import store from './store'
